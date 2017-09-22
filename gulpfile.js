7cf9dbdb5672453f4b8acc7d3eb1ced29869114d
Filippo <filippocolca@gmail.com>
2017-09-22 10:37:44 +0200

    Added new files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+}
+)
+
+gulp.task('html', function() {
+  console.log("Great! You write in HTML");
+}
+)
+
+gulp.task('styles', function() {
+  console.log("Imagine Sass or Post CSS tasks running here");
+}
+)
+
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  })
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  })
+})
