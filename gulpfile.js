6200a4a15bd0dfab426fe6dc4bce6a5e0b7376c0
Filippo Colca <filippo@filippocolca.com>
2017-09-26 17:12:00 +0200

    Update lezione 14

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
-}
-)
+});
 
 gulp.task('html', function() {
-  console.log("Great! You write in HTML");
-}
-)
+  console.log("Imagine something useful being done to your HTML here.");
+});
 
 gulp.task('styles', function() {
-  console.log("Imagine Sass or Post CSS tasks running here");
-}
-)
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
 
 gulp.task('watch', function() {
+
   watch('./app/index.html', function() {
     gulp.start('html');
-  })
+  });
 
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles');
-  })
-})
+  });
+
+});
