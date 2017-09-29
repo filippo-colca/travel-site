697f1179b6fe2a1e1731d974be06b48b21711206
Filippo <filippocolca@gmail.com>
2017-09-29 15:48:48 +0200

    completed large hero block e intro content block

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,7 +5,6 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
-;
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
